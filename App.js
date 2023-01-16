70ff8635d0f741d7ac5e6b0767950a3eaa89f7a4
Felipe Lopes da Silva <flsilva@anp.gov.br>
2023-01-16 16:05:22 -0300

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Desafio
+ */
+import React, { useState } from "react";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
 
 export default function App() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('');
+
+  function cadastrar() {
+    alert('ok')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <View style={styles.viewImagem}>
+        <Image
+          style={styles.imagem}
+          source={require('./src/img/firebase.png')}
+        />
+
+        <Text style={styles.titulo}>Cadastro</Text>
+      </View>
+
+      <View style={styles.body}>
+        <Text style={styles.texto}>Email</Text>
+
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          onChangeText={(texto) => setEmail(texto)}
+          value={email}
+          placeholder="email@gmail.com"
+        />
+
+        <Text style={styles.texto}>Senha</Text>
+
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          onChangeText={(texto) => setPassword(texto)}
+          value={password}
+          placeholder="............."
+        />
+
+        <TouchableOpacity style={styles.botao} onPress={cadastrar}>
+          <Text style={styles.botaoTexto}>Cadastrar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={cadastrar} style={styles.redirecionar}>
+          <Text style={styles.texto}>Clique aqui para realizar o login</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
